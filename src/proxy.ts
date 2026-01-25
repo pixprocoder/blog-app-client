f5c8a1a4c55498fd4706c7ac8f8ad71687ea340a
pixprocoder <makkobir79@gmail.com>
2026-01-25 12:33:35 +0200

    chore: proxy setup doe for protected rotues dashboard

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  console.log("server", request.url);
-  return NextResponse.redirect(new URL("/home", request.url));
+  const pathname = request.nextUrl.pathname;
+  console.log(pathname);
+  let isAuthenticated = true;
+  let isAdmin = true;
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isAdmin && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+  if (!isAdmin && pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/dashboard",
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/admin-dashboard",
+    "/admin-dashboard/:path*",
+  ],
 };
